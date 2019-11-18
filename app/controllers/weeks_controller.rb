f0c2abd24ec829cf1409dca5ebd5f68521f26d5f
Alejandro Reyna <AleejandroReyna@gmail.com>
2019-11-17 18:11:22 -0600

    List and create weeks

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -3,7 +3,23 @@ class WeeksController < ApplicationController
   def  new
   end
 
-  def create
-    render plain: params[:week].inspect
+  def index
+    @weeks = Week.all
   end
+
+  def show
+    @week = Week.find(params[:id])
+  end
+
+  def create
+    @week = Week.new(week_params)
+    print(@week)
+    @week.save
+    redirect_to @week
+  end
+
+  private
+    def week_params
+      params.require(:week).permit(:week)
+    end
 end
---
+++ app/models/week.rb
@@ -0,0 +1,3 @@
+class Week < ApplicationRecord
+  validates_presence_of :week
+end
